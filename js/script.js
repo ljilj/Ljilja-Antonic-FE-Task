b2d9a0ed16041bac22ccaf5b3e7d42e82b213dee
ljilj <ljilja.antonic91@gmail.com>
2021-12-17 21:29:20 +0100

    Added Hero Section

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*** Hero Section ***/ 
+$('.c-hero-slick-slider').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow: '.prevArrow',
+    nextArrow: '.nextArrow'
+});
